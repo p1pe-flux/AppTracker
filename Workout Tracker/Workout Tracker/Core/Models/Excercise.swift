e2bb12673aac18acd7d03f8fdb377582c48cfb80
PipeG09 <103771722+PipeG09@users.noreply.github.com>
2025-06-29 22:42:40 -0300

    Corrijo Workout Tracker para que se suba como carpeta normal

---
+++ Workout Tracker/Workout Tracker/Core/Models/Excercise.swift
@@ -0,0 +1,7 @@
+//
+//  Excercise.swift
+//  Workout Tracker
+//
+//  Created by Felipe Guasch on 29/6/25.
+//
+
---
+++ Workout Tracker/Workout Tracker/Core/Models/Workout.swift
@@ -0,0 +1,7 @@
+//
+//  Workout.swift
+//  Workout Tracker
+//
+//  Created by Felipe Guasch on 29/6/25.
+//
+
